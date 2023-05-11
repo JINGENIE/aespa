da6d14be0f2d1a646febcb29a5bed5f0a8f4d3c3
iiinbo <bo2939@naver.com>
2023-05-11 15:25:53 +0900

    0511

---
+++ src/main/java/com/kbstar/Clone0511Application.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clone0511Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clone0511Application.class, args);
+    }
+
+}
